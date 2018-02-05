ad17a7fe8e141ba3595ea2bbd148672e7a9cddaa
kkeogh <kkeogh@CYGNET.uni.federation.edu.au>
2018-02-05 22:15:18 +1100

    commit 1

---
+++ itech2306Project1/src/itech2306Package1/Person.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package itech2306Package1;
+
+/**
+ * @author kkeogh
+ *
+ */
+public class Person {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
