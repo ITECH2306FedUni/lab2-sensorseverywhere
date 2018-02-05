b63688227211af78fa40161870e4ef92963b2422
kkeogh <kkeogh@CYGNET.uni.federation.edu.au>
2018-02-05 22:27:28 +1100

    added attributes getters and setters

--- itech2306Project1/src/itech2306Package1/Person.java
+++ itech2306Project1/src/itech2306Package1/Person.java
@@ -8,7 +8,32 @@ package itech2306Package1;
  *
  */
 public class Person {
-
+	String name;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	String address;
 	/**
 	 * @param args
 	 */
